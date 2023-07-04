55504f81ee6dc5cad9de768dc817b046032c9994
mrrobot2524 <alenjon2594@gmail.com>
2023-07-04 14:16:08 +0500

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class App {
+    public static boolean scrabble(String symbols, String word) {
+
+        int length = word.length();
+        String[] letters = symbols.split("");
+        List coll = new ArrayList(Arrays.asList(letters));
+
+        for (int i = 0; i < length; i++) {
+            String current = word.substring(i, i + 1).toLowerCase();
+
+            if (!coll.contains(current)) {
+                return false;
+            }
+
+            coll.remove(current);
+        }
+
+        return true;
+    }
+}
